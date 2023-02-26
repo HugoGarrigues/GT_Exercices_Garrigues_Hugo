50b3b7dc9ef1d23007a7c268fdda4116f1bcdf53
HugoGarrigues <113009447+HugoGarrigues@users.noreply.github.com>
2023-02-26 15:22:20 +0100

    Ajout Page 911

---
+++ WEB/JS/carousel_911.js
@@ -0,0 +1,11 @@
+const image = ["../assets/911(1).jpg", "../assets/911(2).jpg", "../assets/911(3).jpg", "../assets/911(4).jpg", "../assets/911(5).jpg"];
+var nombre = 0;
+
+function suivant(sens) {
+    nombre = nombre + sens;
+    if (nombre > image.length - 1)
+        nombre = 0;
+    if (nombre < 0)
+    nombre = image.length - 1;
+    document.getElementById("image").src = image[nombre];
+}
