a9677fa3b334bdb603b7a81a1516555245729c4a
HugoGarrigues <113009447+HugoGarrigues@users.noreply.github.com>
2023-02-26 15:59:48 +0100

    Changement Emplacement Site Web

---
+++ WEB/JS/carousel_index.js
@@ -0,0 +1,11 @@
+const image = ["../../assets/carousel1.webp", "../../assets/carousel2.webp", "../../assets/carousel3.jpg", "../../assets/carousel4.webp"];
+var nombre = 0;
+
+function suivant(sens) {
+    nombre = nombre + sens;
+    if (nombre > image.length - 1)
+        nombre = 0;
+    if (nombre < 0)
+    nombre = image.length - 1;
+    document.getElementById("image").src = image[nombre];
+}
